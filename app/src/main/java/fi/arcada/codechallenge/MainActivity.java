812c64d66fa441386986191e5ea73301a51bdf7f
Viktoriia Mikhailova <mikhailv@arcada.fi>
2025-04-03 22:50:39 +0300

    min första app is ready

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,6 +1,7 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +11,19 @@ import androidx.core.view.WindowInsetsCompat;
 //test
 public class MainActivity extends AppCompatActivity {
 
+    TextView hello;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hello = findViewById(R.id.hello);
+        hello.setText("Min app funkar!");
 
     }
 }
+
+
+
+
 
 
